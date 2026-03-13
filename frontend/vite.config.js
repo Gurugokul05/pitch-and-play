31bc1cc84453b0d0586b0fa28b0876ed9fb2e066
Gurugokul <gurugokul05@gmail.com>
2026-03-13 21:41:20 +0530

    fixed BUILD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || "/",
   build: {
     // Optimize chunk size for better loading
     chunkSizeWarningLimit: 1000,
@@ -34,5 +35,6 @@ export default defineConfig({
   // Enable better tree-shaking
   optimizeDeps: {
     include: ["react", "react-dom", "react-router-dom", "axios"],
+
   },
 });
